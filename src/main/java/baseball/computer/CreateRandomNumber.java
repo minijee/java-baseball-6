dc8e0895a8c833348a423eab1ecbd788566c7d62
jimini <jimin5305@naver.com>
2025-11-30 17:25:38 +0900

    fix: CreateRandomNumber - 랜덤 숫자 생성 수정

--- src/main/java/baseball/computer/CreateRandomNumber.java
+++ src/main/java/baseball/computer/CreateRandomNumber.java
@@ -15,33 +15,11 @@ public class CreateRandomNumber {
      */
     public static List<Integer> generate() {
         list = new ArrayList<>();
-        int idx = 0;
-        int number = 0;
 
-        while(idx < 3) {
-            if(idx == 0) {
-                number = Randoms.pickNumberInRange(1, 9); //첫 번째 자리
-            } else {
-                number = Randoms.pickNumberInRange(0, 9);
-            }
-
-            if(!duplicate(number)) {
-                list.add(number); //새로 생성된 숫자가 중복된 숫자가 아니라면
-                idx++;
-            }
-        }
+        do {
+            list = Randoms.pickUniqueNumbersInRange(0, 9, 3);
+        } while (list.get(0) == 0);
 
         return list;
     }
-
-    /**
-     * 생성된 랜덤 숫자가 중복되는지 확인
-     * @param number 생성된 랜덤 숫자
-     * @return 중복 여부
-     */
-    private static boolean duplicate(int number) {
-
-        return list.contains(number);
-    }
-
 }
