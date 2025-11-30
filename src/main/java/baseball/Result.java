94c491960f4795c5b3fe9196edf3f2460e653e89
jimini <jimin5305@naver.com>
2025-11-30 13:52:07 +0900

    feat: Game 클래스 추가 - start() 메소드 생성

---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Result {
+    private final int ballCount;
+    private final int strikeCount;
+
+    public Result(int ballCount, int strikeCount) {
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+}
