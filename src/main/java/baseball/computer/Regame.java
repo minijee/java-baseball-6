9ee6994b7c71cf32bab4fbafedad36bae1559de6
jimini <jimin5305@naver.com>
2025-11-30 17:05:21 +0900

    feat: Regame 클래스 생성 - 게임 재시작 여부 반환

---
+++ src/main/java/baseball/computer/Regame.java
@@ -0,0 +1,21 @@
+package baseball.computer;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Regame {
+
+    /**
+     * 사용자의 계속 진행 여부를 받아 게임을 재시작 여부 반환
+     * @return 재시작 여부
+     */
+    public static boolean doRegame() {
+        String inputStr = Console.readLine();
+        if(inputStr.equals("1")) {
+            return true;
+        } else if(inputStr.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
