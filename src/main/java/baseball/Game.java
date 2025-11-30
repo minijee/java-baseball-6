b1c4aa7784fe25d925c3bff13562bb2151403299
jimini <jimin5305@naver.com>
2025-11-30 17:10:22 +0900

    feat: Game - 게임 반복 진행 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.Umpire.Judge;
+import baseball.Umpire.JudgeResult;
 import baseball.computer.CreateRandomNumber;
 import baseball.computer.PrintMessage;
+import baseball.computer.ProcessMessage;
+import baseball.computer.Regame;
+import baseball.player.PlayerInput;
+
+import java.util.List;
 
 public class Game {
 
@@ -12,20 +19,36 @@ public class Game {
         //1. 시작 메시지 출력
         PrintMessage.printInfoMessage(ProcessMessage.START.getText());
 
-//        while(true) {
-            //2. 랜덤 3자리 숫자 생성
-            CreateRandomNumber.generate();
+        //2. 랜덤 3자리 숫자 생성
+        List<Integer> randomNumberList = CreateRandomNumber.generate();
+        System.out.println(randomNumberList);
 
+        while(true) {
             //3. 사용자 입력
+            PrintMessage.printInputMessage(ProcessMessage.INPUT.getText());
+            List<Integer> inputNumberList = PlayerInput.inputRandomNumber();
 
-            //4. 입력값 검증
+            //4. 평가 진행
+            JudgeResult judgeResult = Judge.evaluate(randomNumberList, inputNumberList);
 
-            //5. 평가 진행
+            //5. 평가 결과
+            PrintMessage.printJudgeResultMessage(judgeResult);
 
-            //6. 결과 출력
+            //6. 정답 시 계속 진행 여부
+            if(Judge.isCorrectAnswer(judgeResult)) {
+                PrintMessage.printCorrectAnswer();
 
-            //7. 정답 시 계속 진행
-//        }
-    }
+                if(Regame.doRegame()) { //재시작 시
+                    randomNumberList = CreateRandomNumber.generate();
+                    System.out.println(randomNumberList);
+                } else { //종료 시
+                    return;
+                }//end if
 
-}
+            }//end if
+
+        }//end while
+
+    }//end start
+
+}//class
