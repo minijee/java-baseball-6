f8d9b614168c11a80bb0bdeada8ab615c5d31cf3
jimini <jimin5305@naver.com>
2025-11-30 15:18:14 +0900

    feat: PlayerInputValidate - 사용자가 입력한 값 검증 기능 구현

---
+++ src/main/java/baseball/player/PlayerInputValidate.java
@@ -0,0 +1,32 @@
+package baseball.player;
+
+public class PlayerInputValidate {
+
+    /**
+     * 사용자가 입력한 3자리 숫자 조건 검증
+     * @param inputCharArr 입력 받은 배열 
+     * @return 통과 여부
+     */
+    public static boolean isInputRandomNumberValidate(char[] inputCharArr) {
+        //입력 길이 확인
+        if(inputCharArr.length != 3) return false;
+        
+        //숫자 여부 확인
+        for(char input : inputCharArr) {
+          if(input < '0' || input > '9') return false;
+        }
+        
+        return true;
+    }
+
+    /**
+     * 사용자가 입력한 계임 계속 진행 여부 검증
+     * @param inputStr 입력 받은 값
+     * @return 통과 여부
+     */
+    public static boolean isInputRegameValidate(String inputStr) {
+        if(inputStr.equals("1") || inputStr.equals("2")) return true;
+
+        return false;
+    }
+}
