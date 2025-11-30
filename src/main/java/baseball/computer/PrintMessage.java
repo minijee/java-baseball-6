5975d02fc487e49237452d4031e33c15ce08d34d
jimini <jimin5305@naver.com>
2025-11-30 14:12:37 +0900

    feat: PrintMessage 클래스 생성 - 출력 메소드 구현

---
+++ src/main/java/baseball/computer/PrintMessage.java
@@ -0,0 +1,20 @@
+package baseball.computer;
+
+public class PrintMessage {
+
+    /**
+     * 정보 메시지 출력
+     * @param message 출력할 메시지
+     */
+    public static void printInfoMessage(String message) {
+        System.out.println(message);
+    }
+
+    /**
+     * 입력 요청 메시지 출력
+     * @param message 출력할 메시지
+     */
+    public static void printInputMessage(String message) {
+        System.out.print(message);
+    }
+}
