1f9209b8806e14158b4ddd6053bef9584c41eeee
jimini <jimin5305@naver.com>
2025-11-30 14:39:35 +0900

    feat: CreateRandomNumber - 중복 제거 구현

--- src/main/java/baseball/computer/CreateRandomNumber.java
+++ src/main/java/baseball/computer/CreateRandomNumber.java
@@ -15,19 +15,33 @@ public class CreateRandomNumber {
      */
     public static List<Integer> generate() {
         list = new ArrayList<>();
+        int idx = 0;
+        int number = 0;
 
-        for(int i = 0; i < 3; i++) {
-            int number = 0;
-
-            if(i == 0) {
-                number = Randoms.pickNumberInRange(1, 9);
+        while(idx < 3) {
+            if(idx == 0) {
+                number = Randoms.pickNumberInRange(1, 9); //첫 번째 자리
             } else {
                 number = Randoms.pickNumberInRange(0, 9);
             }
 
-            list.add(number);
+            if(!duplicate(number)) {
+                list.add(number); //새로 생성된 숫자가 중복된 숫자가 아니라면
+                idx++;
+            }
         }
 
         return list;
     }
+
+    /**
+     * 생성된 랜덤 숫자가 중복되는지 확인
+     * @param number 생성된 랜덤 숫자
+     * @return 중복 여부
+     */
+    private static boolean duplicate(int number) {
+
+        return list.contains(number);
+    }
+
 }
