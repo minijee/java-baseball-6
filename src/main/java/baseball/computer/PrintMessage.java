fbf25244bc0bf1b44b19aaf3df543559a5fc1ab0
jimini <jimin5305@naver.com>
2025-11-30 17:07:20 +0900

    feat: PrintMessage - 판정 결과 메시지 출력 기능, 정답 메시지 출력 기능 구현

--- src/main/java/baseball/computer/PrintMessage.java
+++ src/main/java/baseball/computer/PrintMessage.java
@@ -1,5 +1,8 @@
 package baseball.computer;
 
+import baseball.Umpire.BaseBallMessage;
+import baseball.Umpire.JudgeResult;
+
 public class PrintMessage {
 
     /**
@@ -16,5 +19,31 @@ public class PrintMessage {
      */
     public static void printInputMessage(String message) {
         System.out.print(message);
+    }
+
+    /**
+     * 판정 결과 메시지 출력
+     * @param judgeResult 판정 결과
+     */
+    public static void printJudgeResultMessage(JudgeResult judgeResult) {
+        int ballCount = judgeResult.getBallCount();
+        int strikeCount = judgeResult.getStrikeCount();
+
+        StringBuilder sb = new StringBuilder();
+        //볼 or 스트라이크 개수가 있는 경우
+        if(ballCount > 0) sb.append(ballCount).append(BaseBallMessage.BALL.getText()).append(" ");
+        if(strikeCount > 0) sb.append(strikeCount).append(BaseBallMessage.STRIKE.getText()).append(" ");
+        //아무것도 없는 경우
+        if(ballCount == 0 && strikeCount == 0) sb.append(BaseBallMessage.NOTHING.getText());
+
+        System.out.println(sb);
+    }
+
+    /**
+     * 정답 메시지 출력
+     */
+    public static void printCorrectAnswer() {
+        System.out.println(ProcessMessage.END.getText());
+        System.out.println(ProcessMessage.REGAME.getText());
     }
 }
